f83a8609e581038fb8d09191520cf751e143e829
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-07-16 15:24:04 +0300

    feat: allow to change swagger relative path, using properties

--- ujar-boot-starter-restful/src/main/java/org/ujar/boot/starter/restful/RestfulAutoConfiguration.java
+++ ujar-boot-starter-restful/src/main/java/org/ujar/boot/starter/restful/RestfulAutoConfiguration.java
@@ -1,12 +1,16 @@
 package org.ujar.boot.starter.restful;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.ujar.boot.starter.restful.swagger.SwaggerConfiguration;
 import org.ujar.boot.starter.restful.web.WebConfig;
 
 @Configuration
 @Import(value = {
     WebConfig.class,
+    SwaggerConfiguration.class
 })
+@EnableConfigurationProperties(ProjectInfoProperties.class)
 public class RestfulAutoConfiguration {
 }
