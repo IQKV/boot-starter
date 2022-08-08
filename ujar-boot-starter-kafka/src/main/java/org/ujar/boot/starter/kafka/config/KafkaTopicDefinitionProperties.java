f3f26d9c732846096e6e511985355c94c24d0b4a
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-08-08 19:33:58 +0300

    feat: use records for props
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- ujar-boot-starter-kafka/src/main/java/org/ujar/boot/starter/kafka/config/KafkaTopicDefinitionProperties.java
+++ ujar-boot-starter-kafka/src/main/java/org/ujar/boot/starter/kafka/config/KafkaTopicDefinitionProperties.java
@@ -2,19 +2,21 @@ package org.ujar.boot.starter.kafka.config;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Map;
-import lombok.Value;
+import lombok.NonNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 import org.ujar.boot.starter.kafka.config.topic.TopicDefinition;
 
-@Value
 @ConstructorBinding
 @ConfigurationProperties(prefix = "ujar.kafka")
 @Validated
 @SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
-public class KafkaTopicDefinitionProperties {
-  Map<String, TopicDefinition> topics;
+public record KafkaTopicDefinitionProperties (@NonNull Map<String, TopicDefinition> topics) {
+
+  public Map<String, TopicDefinition> getTopics() {
+    return topics();
+  }
 
   public TopicDefinition get(String key) {
     return topics.get(key);
--- ujar-boot-starter-restful/src/main/java/org/ujar/boot/starter/restful/ProjectInfoProperties.java
+++ ujar-boot-starter-restful/src/main/java/org/ujar/boot/starter/restful/ProjectInfoProperties.java
@@ -1,19 +1,15 @@
 package org.ujar.boot.starter.restful;
 
 import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 @ConstructorBinding
-@AllArgsConstructor
-@Value
 @Validated
 @ConfigurationProperties("build")
-public class ProjectInfoProperties {
-  @NotNull String version;
-  @NotNull String description;
-  @NotNull String relativePath;
+public record ProjectInfoProperties(@NotNull String version,
+                                    @NotNull String description,
+                                    @NotNull String relativePath) {
+
 }
