e961f043554e915c56160c4913cd8e99d0e2d1c9
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2023-03-31 17:08:46 +0300

    feat: migrate to Java 19, jakarta APIs, Spring Boot 3
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- ujar-boot-starter-amqp/src/main/java/org/ujar/boot/starter/amqp/AmqpAutoConfiguration.java
+++ ujar-boot-starter-amqp/src/main/java/org/ujar/boot/starter/amqp/AmqpAutoConfiguration.java
@@ -1,7 +1,10 @@
 package org.ujar.boot.starter.amqp;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.ujar.boot.build.BuildInfoConfig;
 
 @Configuration
+@Import(BuildInfoConfig.class)
 public class AmqpAutoConfiguration {
 }
