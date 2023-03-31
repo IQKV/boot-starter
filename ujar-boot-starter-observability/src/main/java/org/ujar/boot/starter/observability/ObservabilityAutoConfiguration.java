175ed64d73f688f5ffcf216400a7d8aa1641093e
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2023-03-31 17:08:46 +0300

    feat: migrate to Java 19, jakarta APIs, Spring Boot 3

---
+++ ujar-boot-starter-observability/src/main/java/org/ujar/boot/starter/observability/ObservabilityAutoConfiguration.java
@@ -0,0 +1,7 @@
+package org.ujar.boot.starter.observability;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObservabilityAutoConfiguration {
+}
--- ujar-boot-starter-restful/src/main/java/org/ujar/boot/starter/restful/web/dto/ErrorResponse.java
+++
@@ -1,8 +0,0 @@
-package org.ujar.boot.starter.restful.web.dto;
-
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.util.List;
-
-@SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
-public record ErrorResponse<M>(List<Error<M>> errors) {
-}
