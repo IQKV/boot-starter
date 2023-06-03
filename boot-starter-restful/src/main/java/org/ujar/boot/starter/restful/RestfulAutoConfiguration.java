a6f2afd9defbcac54b46eccae3f5157da7f7cdd5
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2023-06-03 14:41:06 +0300

    chore: enable reasonable defaults for observability components
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- boot-starter-restful/src/main/java/org/ujar/boot/starter/restful/RestfulAutoConfiguration.java
+++ boot-starter-restful/src/main/java/org/ujar/boot/starter/restful/RestfulAutoConfiguration.java
@@ -2,13 +2,11 @@ package org.ujar.boot.starter.restful;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.ujar.boot.restful.swagger.SwaggerConfiguration;
 import org.ujar.boot.restful.web.WebConfig;
 
 @Configuration
 @Import(value = {
-    WebConfig.class,
-    SwaggerConfiguration.class
+    WebConfig.class
 })
 public class RestfulAutoConfiguration {
 }
