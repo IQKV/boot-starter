495487f9e7080878b282eefc7e1973d76855944e
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-07-19 19:06:12 +0300

    feat: update config metadata for security section
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- boot-starter-security/src/main/java/org/iqkv/boot/starter/security/SecurityAutoConfiguration.java
+++ boot-starter-security/src/main/java/org/iqkv/boot/starter/security/SecurityAutoConfiguration.java
@@ -1,7 +1,12 @@
 package org.iqkv.boot.starter.security;
 
+import org.iqkv.boot.security.SecurityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(value = {
+    SecurityProperties.class
+})
 public class SecurityAutoConfiguration {
 }
