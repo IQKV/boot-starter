d26118cb56810aab3d1127d36c6bd8014dce1a5c
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2024-12-01 20:11:14 +0200

    chore: migrate to com.iqkv.boot.web.rest package name

--- boot-starter-mvc-rest/src/main/java/com/iqkv/boot/starter/web/rest/RestfulAutoConfiguration.java
+++ boot-starter-mvc-rest/src/main/java/com/iqkv/boot/starter/web/rest/RestfulAutoConfiguration.java
@@ -1,6 +1,6 @@
 package com.iqkv.boot.starter.web.rest;
 
-import com.iqkv.boot.web.rest.WebConfig;
+import com.iqkv.boot.mvc.rest.WebConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
