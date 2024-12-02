a88a69960173a3dd78837326ebba96b213901d63
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2024-12-02 20:42:27 +0200

    style: fix import order

--- boot-starter-cache/src/main/java/com/iqkv/boot/starter/cache/CacheAutoConfiguration.java
+++ boot-starter-cache/src/main/java/com/iqkv/boot/starter/cache/CacheAutoConfiguration.java
@@ -1,7 +1,7 @@
 package com.iqkv.boot.starter.cache;
 
-import com.iqkv.boot.info.BuildInfoConfig;
 import com.iqkv.boot.cache.CacheProperties;
+import com.iqkv.boot.info.BuildInfoConfig;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
