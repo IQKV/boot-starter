7cae1daa7bdbda717bec9f01a019c61c111ae8db
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-01-30 19:10:18 +0200

    chore: update streamline version and copyright headers

--- boot-starter-security/src/main/java/com/iqkv/boot/starter/security/SecurityAutoConfiguration.java
+++ boot-starter-security/src/main/java/com/iqkv/boot/starter/security/SecurityAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 IQKV Team, and the original author or authors from the JHipster project.
+ * Copyright 2025 IQKV Team, and the original author or authors from the JHipster project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
