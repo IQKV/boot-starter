19d987665b0b4b746f63e0cfcbaf9a9b0e1b6fb5
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-05-28 13:01:55 +0300

    chore: package structure housekeeping

---
+++ boot-starter-cache/src/main/java/com/iqkv/boot/starter/cache/CacheAutoConfiguration.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2025 Expertness Team, and the original author or authors from the JHipster project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.iqkv.boot.starter.cache;
+
+import com.iqkv.boot.cache.CacheProperties;
+import com.iqkv.boot.info.BuildInfoConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(BuildInfoConfig.class)
+@EnableConfigurationProperties(CacheProperties.class)
+public class CacheAutoConfiguration {
+
+}
