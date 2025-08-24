dbbf82a7ca2097dfc9d4328804dfd25de9bc2227
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-08-24 17:13:27 +0300

    style: format source accordingly to the guide

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,23 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "footer-max-line-length": [1, "always", 180],
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "fix",
-        "rfc",
-        "docs",
-        "style",
-        "refactor",
-        "perf",
-        "test",
-        "chore",
-        "build",
-        "ci",
-        "revert",
-      ],
-    ],
+    "type-enum": [2, "always", ["feat", "fix", "rfc", "docs", "style", "refactor", "perf", "test", "chore", "build", "ci", "revert"]],
   },
 };
